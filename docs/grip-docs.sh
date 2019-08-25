02077cea34de973f76335a59d13a0cf90629c0df
Athanasius <code@miggy.org>
2019-08-25 13:19:38 +0100

    grip-docs.sh: Only run grip if .md is newer than .html

--- docs/grip-docs.sh
+++ docs/grip-docs.sh
@@ -4,6 +4,9 @@ DOCS="FrontierDevelopments-oAuth2-notes.md"
 for d in ${DOCS};
 do
 	HTMLDOC=$(basename "${d}" .md).html
-	grip "${d}" --export "${HTMLDOC}" \
-		&& chmod 644 "${HTMLDOC}"
+	if [ "${d}" -nt "${HTMLDOC}" ];
+	then
+		grip "${d}" --export "${HTMLDOC}" \
+			&& chmod 644 "${HTMLDOC}"
+	fi
 done
