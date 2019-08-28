0b31b0bcdaf2fb1a66a2520d8b99227072360225
Athanasius <code@miggy.org>
2019-08-28 12:04:25 +0100

    Adds some response checking to profile code.

--- edcapi/profile/profile.py
+++ edcapi/profile/profile.py
@@ -25,8 +25,17 @@ class profile(object):
         "Content-Type": "application/json"
       }
     )
-    self.__profile = json.loads(response.text)
-    self.__db.updateLastSuccessfulUse(cmdrname, access_token)
+    self.__profile = None
+    if response.status_code == 200:
+      self.__profile = json.loads(response.text)
+      self.__db.updateLastSuccessfulUse(cmdrname, access_token)
+      self.__logger.debug("Success, headers = \n{}".format(response.headers))
+    elif response.status_code == 206:
+      self.__logger.error("Got 206, but this isn't a journal request!")
+    elif r_status_code == 422: # Unprocessable Entity
+      self.__logger.warn("HTTP Status 422 - Access Token expired?")
+    else:
+      self.__logger.critical("Got HTTPS Status: {}".format(response.status_code))
 
     return self.__profile
 ###########################################################################
