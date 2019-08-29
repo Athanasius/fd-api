4d2d23c8ccf68f778ee1dfcd3e83dc651ce219da
Athanasius <code@miggy.org>
2019-08-29 15:02:23 +0100

    docs: move TODO into docs directory

--- docs/grip-docs.sh
+++ docs/grip-docs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # vim: textwidth=0 wrapmargin=0 shiftwidth=2 tabstop=2 softtabstop smartindent
 
-DOCS="FrontierDevelopments-oAuth2-notes.md FrontierDevelopments-CAPI-endpoints.md ../TODO.md"
+DOCS="FrontierDevelopments-oAuth2-notes.md FrontierDevelopments-CAPI-endpoints.md TODO.md"
 for d in ${DOCS};
 do
   HTMLDOC=$(basename "${d}" .md).html
