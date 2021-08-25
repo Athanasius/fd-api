9c70c78034e92ca16710f5c491f5fd3d8710dc7e
Athanasius <code@miggy.org>
2021-08-25 15:33:16 +0100

    market: white space around conditionals

--- org/miggy/edcapi/market.py
+++ org/miggy/edcapi/market.py
@@ -30,6 +30,7 @@ class market(object):
     if not access_token:
       self.__logger.critical("No Access Token, you'll need to auth from scratch again")
       return None
+
     # Send request with Access Token
     uri = self.__config.get('capi_url') + '/market'
     response = requests.get(uri, stream=True,
@@ -50,16 +51,20 @@ class market(object):
       self.__market = json.loads(self.__raw_market)
       self.__db.updateLastSuccessfulUse(cmdrname, access_token)
       self.__logger.debug("Success\nConnected To: {}\nHeaders:\n{}".format(peer, response.headers))
+
     elif response.status_code == 206:
       self.__logger.error("Got 206, but this isn't a journal request!")
+
     elif response.status_code in [ 401, 422 ]:
       # NB: CAPI servers used to return '422 - unprocessable entity' when
       #     the Access Token you'd tried to use was expired.  After the
       #     'September Update' patch in Sep 2019 they changed to using
       #     '401 - unauthorised' for this.
       self.__logger.warn("HTTP Status {} - Access Token expired".format(response.status_code))
+
     elif response.status_code == 418: # I'm a teapot - down for maintenance
       self.__logger.critical("HTTP Status 418 - Servers probably down for maintenance: %s", response.text)
+
     else:
       self.__logger.critical("Got HTTPS Status: {}".format(response.status_code))
 
